69b8a640fdb1ec27123dbd4581b7ad953dfb6d80
Andrew <a.balanyuk@yahoo.com>
2016-12-04 17:38:43 +0200

    Add Redux boilerplate

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,43 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: "" }
+    };
+    // better performance since if used in render
+    // it would create a newly bound function each time it renders
+    this.onClickSave = this.onClickSave.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <h3>{this.state.course.title}</h3>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
