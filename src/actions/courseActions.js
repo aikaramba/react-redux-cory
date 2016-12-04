3277478254ea04ed7a091c93ed9e8b995e086f1c
Andrew <a.balanyuk@yahoo.com>
2016-12-04 19:11:53 +0200

    Add actionTypes constants file

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,5 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course }; // short-hand property name
+  return { type: types.CREATE_COURSE, course }; // short-hand property name
 }
