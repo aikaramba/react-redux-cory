821d8b5dd5389631578a87f238607f0083b3a703
Andrew <a.balanyuk@yahoo.com>
2016-12-11 19:21:49 +0200

    Add mock API and actions & Create display table for courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,31 +2,15 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-    // better performance since if used in render
-    // it would create a newly bound function each time it renders
-    this.onClickSave = this.onClickSave.bind(this);
-    this.onTitleChange = this.onTitleChange.bind(this);
+    // better performance if binding functions here
+    // if used in render method it would create
+    // a newly bound function each time class renders
     this.courseRow = this.courseRow.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    // raw way to dispatch an action
-    //this.props.dispatch(courseActions.createCourse(this.state.course));
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -34,20 +18,12 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
+        <CourseList courses={courses}/>
       </div>
     );
   }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,8 +2,10 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type){
-    case types.CREATE_COURSE:
-      return [...state, Object.assign({}, action.course)];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
+    // just returning our new courses array since it will
+    // replace everything that was in courses state before
 
     default:
       return state;
